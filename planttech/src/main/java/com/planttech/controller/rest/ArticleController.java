380774abae97396aca9d7b2cfe2fb8203ba06964
Dayoung Han <dayounghan@Dayoungui-MacBookPro.local>
2023-04-11 10:37:03 +0900

    feat: Add auth & article controller

---
+++ planttech/src/main/java/com/planttech/controller/rest/ArticleController.java
@@ -0,0 +1,40 @@
+package com.planttech.controller.rest;
+
+import java.util.List;
+
+import com.planttech.domain.PlantSensor;
+import com.planttech.domain.Page;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.planttech.service.PlantSensorService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Article", description = "재배 식물 거래 API")
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+	
+	@Autowired
+	private PlantSensorService PlantSensorService;
+	
+	@GetMapping
+	public int getArticleList() {
+		
+		System.out.println("::: GET - getArticleList :::");
+
+		return 0;
+	}
+	
+	
+	
+	
+	
+}
