7fe9485cb5b6661e370bcc7696ff3290874f032c
Dayoung Han <dayounghan@Dayoungui-MacBookPro.local>
2023-04-11 10:35:40 +0900

    chore: Add user no in page DTO

--- planttech/src/main/java/com/planttech/domain/PlantSensor.java
+++ planttech/src/main/java/com/planttech/domain/PlantSensor.java
@@ -8,6 +8,7 @@ public class PlantSensor {
 
 	private int plantSensorNo;
 	private int plantNo;
+	private int warehousePlantNo;
 	private int dhtNo;
 	private int photoRegistorNo;
 	private int waterTempNo;
