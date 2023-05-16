9b7cb1ee8820a537ad748c34b8a340d599bf5264
KangHeeKwon <khk680875@gmail.com>
2023-05-16 10:14:46 +0900

    MY 페이지의 "내 프로필" 메인 화면 구현, SHOP 화면의 리스트 아이템 추가

--- Plant/app/src/main/java/com/example/plant/Fragment/InfoProfileFragment.kt
+++ Plant/app/src/main/java/com/example/plant/Fragment/InfoProfileFragment.kt
@@ -5,16 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.plant.R
+import com.example.plant.adapter.BoardAdapter
+import com.example.plant.adapter.InfoProfileAdapter
+import com.example.plant.databinding.FragmentInfoProfileBinding
+import com.example.plant.model.Board
 
 class InfoProfileFragment : Fragment() {
-
+    lateinit var recyclerView : RecyclerView
+    lateinit var infoProfileAdapter: InfoProfileAdapter
+    private lateinit var data : List<Board>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_profile, container, false)
+        val binding = FragmentInfoProfileBinding.inflate(inflater, container, false)
+
+        data = mutableListOf(Board(
+            0, 0, 0,
+            "토마토", "Good", "5Kg, 당도 높음", "50,000원", "13:12", "1",
+        ),
+            Board(
+                1, 0, 0,
+                "부레옥잠", "Soso", "수상생물", "30,000원", "2023-05-10", "1",
+            ),
+            Board(
+                2, 0, 0,
+                "난", "Good", "1M, 사진 참고", "100,000원", "2023-05-08", "1",
+            ),
+            Board(
+                0, 0, 0,
+                "토마토", "Good", "5Kg, 당도 높음", "50,000원", "13:12", "1",
+            ),
+            Board(
+                1, 0, 0,
+                "부레옥잠", "Soso", "수상생물", "30,000원", "2023-05-10", "1",
+            ),
+            Board(
+                2, 0, 0,
+                "난", "Good", "1M, 사진 참고", "100,000원", "2023-05-08", "1",
+            ))
+
+        infoProfileAdapter = context?.let { InfoProfileAdapter(data) }!!
+        binding.infoProfileMysellRcv.adapter = infoProfileAdapter
+
+        // val intent = Intent(this.context, CameraFragment::class.java)
+        recyclerView = binding.infoProfileMysellRcv
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        recyclerView.adapter = InfoProfileAdapter(data)
+        binding.infoProfileMysellRcv.setHasFixedSize(true)
+
+        return binding.root
     }
 
 }
---
+++ Plant/app/src/main/java/com/example/plant/adapter/InfoProfileAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.plant.adapter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.plant.Fragment.BoardContentFragment
+import com.example.plant.Fragment.CameraFragment
+import com.example.plant.R
+import com.example.plant.model.Board
+import com.example.plant.model.Plant
+
+class InfoProfileAdapter(var mylist: List<Board>) : RecyclerView.Adapter<InfoProfileAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_plant, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return mylist.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.name.text = mylist[position].articleTitle
+        holder.price.text = mylist[position].articleProductPrice
+        holder.variety.text = mylist[position].articleContent
+        holder.state.text = mylist[position].articleSubject
+
+        /*
+        holder.itemView.setOnClickListener { v ->
+            val activity = v!!.context as AppCompatActivity
+            val bundle = Bundle()
+            bundle.putString("title", mylist[position].articleTitle)
+            bundle.putString("createTime", mylist[position].articleCreatetime)
+            bundle.putString("content", mylist[position].articleContent)
+            bundle.putString("price", mylist[position].articleProductPrice)
+            val boardContentFragment = BoardContentFragment()
+            boardContentFragment.arguments = bundle
+            activity.supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainer, boardContentFragment)
+                .addToBackStack(null)
+                .commit()
+        }*/
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val name: TextView = itemView.findViewById(R.id.shop_item_name)
+        val price: TextView = itemView.findViewById((R.id.shop_item_price))
+        val variety: TextView = itemView.findViewById(R.id.shop_item_variety)
+        val state: TextView = itemView.findViewById(R.id.shop_item_state)
+    }
+
+    interface ItemClickListener{
+        fun onClick(view: View,position: Int)
+    }
+    //를릭 리스너
+    private lateinit var itemClickListner: ItemClickListener
+    fun setItemClickListener(itemClickListener: ItemClickListener) {
+        this.itemClickListner = itemClickListener
+    }
+
+}
