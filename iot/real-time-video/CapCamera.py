48285f8266b128309c152d5b87fd8506bd51939b
little6523 <67815747+little6523@users.noreply.github.com>
2023-05-21 04:54:18 +0900

    Update CapCamera.py

--- iot/real-time-video/CapCamera.py
+++ iot/real-time-video/CapCamera.py
@@ -10,6 +10,7 @@ import json
 
 from State import *
 from remove_bg import *
+from rem_bg import *
 
 sio = socketio.Client()
 
@@ -25,40 +26,47 @@ def message(data):
 
 class camera :
     frame = None
-    frame2 = None
     state = None
-    state2 = None
+    cam_list = [] # 연결된 카메라 리스트
+    cam_num = 0
+    img_path = r'C:\Users\user\Desktop\SendCamera\SendCamera'
     now = datetime.now()
 
+    def connect_camera(self) : # 연결된 카메라 검사 함수
+        self.cam_num = 0
+        while True :
+            cam = cv2.VideoCapture(self.cam_num)
+            if not cam.isOpened() :
+                break
+            self.cam_list.append(cam) # 카메라 추가
+            print("%d번 카메라 추가" %(self.cam_num))
+            self.cam_num += 1
+
     # Flask-SocketIO 서버로 영상을 전송하는 함수
-    def send_video(self, frame, frame2):
+    def send_video(self, frame, cam_index):
         if frame is not None :
-            # 영상 프레임을 base64로 인코딩하여 문자열로 변환합니다.
+            # 영상 프레임을 base64로 인코딩하여 문자열로 변환
             _, buffer = cv2.imencode('.jpg', frame)
             data = base64.b64encode(buffer).decode('utf-8')
 
-            # 'video' 이벤트를 사용하여 영상 데이터를 Flask-SocketIO 서버로 전송합니다.
-            sio.emit('video1', data)
-
-        if frame2 is not None :
-            _, buffer = cv2.imencode('.jpg', frame2)
-            data = base64.b64encode(buffer).decode('utf-8')
-
-            # 'video' 이벤트를 사용하여 영상 데이터를 Flask-SocketIO 서버로 전송합니다.
-            sio.emit('camera2', data)
+            # 'video' 이벤트를 사용하여 영상 데이터를 Flask-SocketIO 서버로 전송
+            event_name = "camera" + str(cam_index-1) # 추후 수정<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            sio.emit(event_name, data)
 
     def get_today_state(self) : # 하루에 한 번씩 정해진 시간마다 상태 얻기
+        for i in range(self.cam_num) :
+            _, frame = self.cam_list[i].read()
+            file_name = self.img_path + '\\test' + str(i) + '.png'
+            cv2.imwrite(file_name, frame)
 
-         # self.capture()
-         rm_bg()
-         self.state, avg_color = get_state()
-         print(self.state)
-         print(avg_color)
+            rem_file_name = remove_bg(self.img_path, file_name, i)
 
-         sio.emit('state', self.state)
-         #self.send_api()
+            self.state, avg_color = get_state(rem_file_name)
+            print(self.state)
+            print(avg_color)
 
-         self.flag = 1
+        sio.emit('state', self.state)
+        #self.send_api()
 
     def send_api(self) : # api로 식물 상태 데이터 보내는 함수
 
@@ -71,62 +79,45 @@ class camera :
 
         print("response: ", response)
         print("response.text: ", response.text)
-
-    def capture(self) : # 이미지 저장
-        cv2.imwrite(r'C:\Users\user\Desktop\SendCamera\SendCamera\today_plant.png', self.frame)
-        cv2.imwrite(r'C:\Users\user\Desktop\SendCamera\SendCamera\today_plant2.png', self.frame2)
             
     def cap_camera(self) :
 
+        self.connect_camera()
         # IP 카메라 영상 호출
         #cap = cv2.VideoCapture('rtsp://admin:uoclab2023@192.168.0.36:10554/tcp/av0_0')
     
-        cap = cv2.VideoCapture(0)
-        cap2 = cv2.VideoCapture(1)
-        count = 0
-    
-        while True:
-            ret, self.frame = cap.read()
-            ret2, self.frame2 = cap2.read()
-            self.now = datetime.now()
-        
-            if ret:
-        
-                height, width = self.frame.shape[:2]
-
-                height2, width2 = self.frame2.shape[:2]
-        
-                # 줄일 해상도
-                new_height, new_width = height // 2, width // 2
-
-                new_height2, new_width2 = height2 // 2, width2 // 2
-        
-                # 프레임의 해상도를 줄임
-                self.frame = cv2.resize(self.frame, (new_width, new_height))
-
-                self.frame2 = cv2.resize(self.frame2, (new_width2, new_height2))
-    
-                self.send_video(self.frame, self.frame2)
-                time.sleep(0.05)
+        while True :
+            for cam_index in range(self.cam_num) :
+                ret, frame = self.cam_list[cam_index].read()
+                if ret :
+                    height, width = frame.shape[:2]
                 
-            else:
-                print('카메라 오류')
-                break
-    
-        cap.release()
-        cv2.destroyAllWindows()
+                    # 줄일 해상도
+                    new_height, new_width = height // 2, width // 2
+                
+                    # 프레임의 해상도를 줄임
+                    frame = cv2.resize(frame, (new_width, new_height))
+        
+                    self.send_video(frame, cam_index)
+        
+                    time.sleep(0.05)
+
 
     def run(self) :
         
         th1 = Thread(target=self.cap_camera)
 
-        schedule.every().day.at("13:30:00").do(self.capture) # 매일 정해진 시간에 캡쳐 => 나중에 지우고 get_today_state함수에 추가
-        schedule.every().day.at("13:32:00").do(self.get_today_state) # 매일 정해진 시간에 상태 출력
+        schedule.every().day.at("04:43:50").do(self.get_today_state) # 매일 정해진 시간에 상태 출력
 
         th1.start()
         while True :
             schedule.run_pending()
-            time.sleep(1)
+            time.sleep(0.05)
+
+        for i in range(len(self.cam_list)) :
+            self.cam_list[i].release
+
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__" :
     sio.connect('http://220.68.82.79:4000')     # Nas의 Flask-SocketIO 서버에 연결
