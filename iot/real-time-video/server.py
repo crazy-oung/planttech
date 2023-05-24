01108195587cdd1b535e0b0a305ef57c74c98d58
little6523 <67815747+little6523@users.noreply.github.com>
2023-05-24 11:01:20 +0900

    Update server.py

--- iot/real-time-video/server.py
+++ iot/real-time-video/server.py
@@ -1,11 +1,7 @@
-from datetime import datetime
-from flask import Flask, Response, request, send_file
-from flask_socketio import SocketIO, emit, send
+from flask import Flask
+from flask_socketio import SocketIO, emit
 from flask_cors import CORS
-import base64
-import cv2
-import numpy as np
-import eventlet
+from threading import Thread
 
 # Flask-SocketIO
 app = Flask(__name__)
@@ -14,13 +10,26 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 # Flask-CORS
 CORS(app)
 
+init_data1 = None
+init_data2 = None
+
 # handle video1 event
-@socketio.on('video1')
+@socketio.on('camera0')
 def handle_video1(data):
-	
-    data = base64.b64decode(data.encode('utf-8'))
-    socketio.emit('video2', data)
+
+    if data is not None :
+        socketio.emit('video2', data)
+    
+@socketio.on('camera1')
+def handle_video2(data):
+    
+    if data is not None :
+        socketio.emit('test', data)
 
 # Flask-SocketIO
 if __name__ == '__main__':
+    th1 = Thread(target=handle_video1(init_data1))
+    th2 = Thread(target=handle_video2(init_data2))
+    th1.start()
+    th2.start()
     socketio.run(app, host='0.0.0.0', port=4000, debug=False)
